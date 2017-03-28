c94440cf7c6d10a20adfba76d7237a1b8a5e2f9c
SASSI Yasser <SASSI Yasser>
2017-03-28 09:53:58 +0200

    Constantes pour representants les differents types d'action à executer après la creation d'un mvmt RH.

---
+++ actionMvmt.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class ActionMvmt{
+	const AVENANT_DRH = AvenantDRH;
+	const AVENANT_DRI = AvenantDRI;
+	const AVENANT_DSI = AvenantDSI;
+	const SORTIE_DRH = SortieDRH;
+	const SORTIE_DRI = SortieDRI;
+	const SORTIE_DSI = SortieDSI;
+	const SORTIE_PAP = SortiePAP;
+	const ENTREE_DRH = EntreeDRH;
+	const ENTREE_DRI = EntreeDRI;
+	const ENTREE_DSI = EntreeDSI;
+	const ENTREE_PAP = EntreePAP;
+	const ENTREE_PROLONGATION_DRH = EntreeProlongationDRH;
+	const ENTREE_PROLONGATION_DRI = EntreeProlongationDRI;
+	const ENTREE_PROLONGATION_DSI = EntreeProlongationDSI;
+	const ENTREE_PROLONGATION_PAP = EntreeProlongationPAP;
+	const ENTREE_REMPLACEMENT_PAP = EntreeRemplacementPAP;
+	const AJOUT_PP = AjoutPP;
+	const MAIL_DRH = MailDRH;
+	const MAIL_DRI = MailDRI;
+	const MAIL_DSI = MailDSI;
+	const MAIL_PAP = MailPAP;
+	const MAIL_CDD_CDI_DRH = MailCDDCDIDRH;
+	const SORTIE_PROLONGATION_DRH = SortieProlongationDRH;
+	const SORTIE_PROLONGATION_DRI = SortieProlongationDRI;
+	const SORTIE_PROLONGATION_DSI = SortieProlongationDSI;
+	const SORTIE_PROLONGATION_PAP = SortieProlongationPAP;
+}
+
+?>
